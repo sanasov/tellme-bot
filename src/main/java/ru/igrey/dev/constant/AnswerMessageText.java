680f8ad9133f365c1c35dc0ebb3c8f254647f587
sanasov <igrey@mail.ru>
2017-08-19 13:31:17 +0300

    design

--- src/main/java/ru/igrey/dev/constant/AnswerMessageText.java
+++ src/main/java/ru/igrey/dev/constant/AnswerMessageText.java
@@ -1,7 +1,7 @@
 package ru.igrey.dev.constant;
 
 public class AnswerMessageText {
-    public final static String ADD_NOTE_AND_PICK_CATEGORY = "Отправьте мне заметку и выберите для нее категорию. \n Команды: \n /shownotes \n /deletenotes";
+    public final static String ADD_NOTE_AND_PICK_CATEGORY = "Отправьте мне заметку и выберите для нее категорию. \n Команды: \n /shownotes - просмотр записей \n /deletenotes - удаление записей";
     public final static String CATEGORY_IS_ADDED = "Категория добавлена";
     public final static String ADD_CATEGORY = "Добавьте категорию";
     public final static String IN_WHICH_CATEGORY_REMOVE_NOTES = "В какой категории удалить записи?";
