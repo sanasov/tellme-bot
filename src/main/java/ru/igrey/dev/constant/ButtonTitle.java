23442924a43c6101b1fe65a1567e96ce90bdb373
sanasov <igrey@mail.ru>
2017-08-30 16:05:48 +0300

    add " back to category

--- src/main/java/ru/igrey/dev/constant/ButtonTitle.java
+++ src/main/java/ru/igrey/dev/constant/ButtonTitle.java
@@ -4,6 +4,6 @@ public class ButtonTitle {
 
     public final static String COMPLETE_DELETING = "Завершить удаление";
     public final static String REMOVE_MODE = "Процесс удаления";
-    public final static String BACK_TO_CATEGORY_VIEW = "<< Назад к категориям";
+    public final static String BACK_TO_CATEGORY_VIEW = "« Назад к категориям";
     public static final String CREATE_CATEGORY = "Создать категорию";
 }
