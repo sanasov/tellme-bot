4e1bac2b0b60a40fd851c165715070b6e9a62eda
sanasov <igrey@mail.ru>
2017-09-06 11:24:50 +0300

    localization npe

--- src/main/java/ru/igrey/dev/Localization.java
+++ src/main/java/ru/igrey/dev/Localization.java
@@ -1,11 +1,13 @@
 package ru.igrey.dev;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Localization {
 
     private static String lang;
 
     public static String get() {
-        return lang;
+        return StringUtils.isNotBlank(lang) ? lang : "en";
     }
 
     public static void set(String languageCode) {
