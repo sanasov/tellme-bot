c951cfb8cb813e4c9a67fc429ccb8ed400520295
sanasov <igrey@mail.ru>
2017-09-11 18:19:03 +0300

    can parse day of week

--- src/main/java/ru/igrey/dev/notifyrule/parse/ParsedDate.java
+++ src/main/java/ru/igrey/dev/notifyrule/parse/ParsedDate.java
@@ -2,7 +2,6 @@ package ru.igrey.dev.notifyrule.parse;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,7 +33,7 @@ public class ParsedDate {
                 || rule.startsWith("in")) {
 
         }
-        List<DayOfWeek> daysOfWeek = retrieveDaysOfWeek();
+        List<DayOfWeek> daysOfWeek = new ParsedDaysOfWeek(rule).get();
         if (daysOfWeek.size() > 0) {
             return daysOfWeek.stream()
                     .map(dayOfWeek -> getNearestLocalDateByDayOfWeek(dayOfWeek))
@@ -52,107 +51,5 @@ public class ParsedDate {
         throw new RuntimeException("Could not get nearest date DayOfWeek:" + dayOfWeek);
     }
 
-    private List<DayOfWeek> retrieveDaysOfWeek() {
-        List<DayOfWeek> daysOfWeek = new ArrayList<>();
-        if (onMonday()) {
-            daysOfWeek.add(DayOfWeek.MONDAY);
-        }
-        if (onTuesday()) {
-            daysOfWeek.add(DayOfWeek.TUESDAY);
-        }
-        if (onWednesday()) {
-            daysOfWeek.add(DayOfWeek.WEDNESDAY);
-        }
-        if (onThursday()) {
-            daysOfWeek.add(DayOfWeek.THURSDAY);
-        }
-        if (inFriday()) {
-            daysOfWeek.add(DayOfWeek.FRIDAY);
-        }
-        if (onSaturday()) {
-            daysOfWeek.add(DayOfWeek.SATURDAY);
-        }
-        if (onSunday()) {
-            daysOfWeek.add(DayOfWeek.SUNDAY);
-        }
-        return daysOfWeek;
-    }
-
-    private Boolean onMonday() {
-        if (rule.contains("понедельник")
-                || rule.contains("пн.")
-                || rule.contains("monday")
-                || rule.contains("mon.")
-                ) {
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean onTuesday() {
-        if (rule.contains("вторник")
-                || rule.contains("вт.")
-                || rule.contains("tuesday")
-                || rule.contains("tues.")
-                ) {
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean onWednesday() {
-        if (rule.contains("сред")
-                || rule.contains("ср.")
-                || rule.contains("wednesday")
-                || rule.contains("wed.")
-                ) {
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean onThursday() {
-        if (rule.contains("четверг")
-                || rule.contains("чт.")
-                || rule.contains("thursday")
-                || rule.contains("thurs.")
-                ) {
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean inFriday() {
-        if (rule.contains("пятниц")
-                || rule.contains("пт.")
-                || rule.contains("friday")
-                || rule.contains("fri.")
-                ) {
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean onSaturday() {
-        if (rule.contains("суббот")
-                || rule.contains("сб.")
-                || rule.contains("saturday")
-                || rule.contains("sat.")
-                ) {
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean onSunday() {
-        if (rule.contains("воскрес")
-                || rule.contains("вс.")
-                || rule.contains("sunday")
-                || rule.contains("sun.")
-                ) {
-            return true;
-        }
-        return false;
-    }
 
 }
---
+++ src/main/java/ru/igrey/dev/notifyrule/parse/ParsedDaysOfWeek.java
@@ -0,0 +1,117 @@
+package ru.igrey.dev.notifyrule.parse;
+
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParsedDaysOfWeek {
+
+    String rule;
+
+    public ParsedDaysOfWeek(String rule) {
+        this.rule = rule;
+    }
+
+    public List<DayOfWeek> get() {
+        List<DayOfWeek> daysOfWeek = new ArrayList<>();
+        if (onMonday()) {
+            daysOfWeek.add(DayOfWeek.MONDAY);
+        }
+        if (onTuesday()) {
+            daysOfWeek.add(DayOfWeek.TUESDAY);
+        }
+        if (onWednesday()) {
+            daysOfWeek.add(DayOfWeek.WEDNESDAY);
+        }
+        if (onThursday()) {
+            daysOfWeek.add(DayOfWeek.THURSDAY);
+        }
+        if (onFriday()) {
+            daysOfWeek.add(DayOfWeek.FRIDAY);
+        }
+        if (onSaturday()) {
+            daysOfWeek.add(DayOfWeek.SATURDAY);
+        }
+        if (onSunday()) {
+            daysOfWeek.add(DayOfWeek.SUNDAY);
+        }
+        return daysOfWeek;
+    }
+
+    private Boolean onMonday() {
+        if (rule.contains("понедельник")
+                || rule.contains("пн.")
+                || rule.contains("monday")
+                || rule.contains("mon.")
+                ) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean onTuesday() {
+        if (rule.contains("вторник")
+                || rule.contains("вт.")
+                || rule.contains("tuesday")
+                || rule.contains("tues.")
+                ) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean onWednesday() {
+        if (rule.contains("сред")
+                || rule.contains("ср.")
+                || rule.contains("wednesday")
+                || rule.contains("wed.")
+                ) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean onThursday() {
+        if (rule.contains("четверг")
+                || rule.contains("чт.")
+                || rule.contains("thursday")
+                || rule.contains("thurs.")
+                ) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean onFriday() {
+        if (rule.contains("пятниц")
+                || rule.contains("пт.")
+                || rule.contains("friday")
+                || rule.contains("fri.")
+                ) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean onSaturday() {
+        if (rule.contains("суббот")
+                || rule.contains("сб.")
+                || rule.contains("saturday")
+                || rule.contains("sat.")
+                ) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean onSunday() {
+        if (rule.contains("воскрес")
+                || rule.contains("вс.")
+                || rule.contains("sunday")
+                || rule.contains("sun.")
+                ) {
+            return true;
+        }
+        return false;
+    }
+}
